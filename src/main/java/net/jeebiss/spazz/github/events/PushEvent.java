b0673146df89de5eede6e2e627009ca509513d03
Morphan1 <morphanone@gmail.com>
2015-03-09 02:10:03 -0400

    Fix some branch announcements
    I've been meaning to do this for a while

--- src/main/java/net/jeebiss/spazz/github/events/PushEvent.java
+++ src/main/java/net/jeebiss/spazz/github/events/PushEvent.java
@@ -20,7 +20,7 @@ public class PushEvent extends Event {
     public class Payload {
         private String ref;
         private List<Commit> commits;
-        public String getBranch() { return ref.substring(ref.lastIndexOf('/')+1); };
+        public String getBranch() { return ref.startsWith("refs/heads/") ? ref.substring(11) : ref; };
         public List<Commit> getCommits() { return commits; }
     }
 
